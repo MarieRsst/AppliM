52641e00441b0630ff9fa1f7c48afd697fd9d0ff
MarieRsst <rassat.marie@gmail.com>
2012-10-04 13:09:15 +0200

    Ajouts scripts

---
+++ Public/Scripts/app.js
@@ -0,0 +1,13 @@
+function check_name(name){
+	if( name == "gabriel"){
+		return "Win !";
+	}else if ( name == "Clement"){
+		return "We find Clement !";
+	}else{
+		var res = "Truth ! " + name + " is different from gabriel and Clement !";
+		return res
+	}
+}
+$ (document).ready(function(){
+	alert(check_name("gabriel"));
+});
